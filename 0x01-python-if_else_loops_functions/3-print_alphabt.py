59d2ae338ab063f1ba8387427254211dc4a6c08c
Natalia Ponsard <2503@holbertonschool.com>
2020-12-08 17:40:59 +0000

    fixed 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for a in range(97, 123):
     if a != 113 and a != 101:
-#      print(chr(a), end = "")
-       print("{}".format(chr(a)), end='')
+        print("{}".format(chr(a)), end='')
