a698472c80283a8cbd503a4f34b0dba43abeaa15
Natalia Ponsard <2503@holbertonschool.com>
2020-12-10 10:21:47 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+argc = len(argv)
+if argc == 1:
+    print("0 arguments.")
+elif argc == 2:
+    print("{} argument:".format(argc - 1))
+else:
+    print("{} argument:".format(argc - 1))
+
+for i in range(1, argc):
+    print("{}: {}".format(i, argv[i]))
