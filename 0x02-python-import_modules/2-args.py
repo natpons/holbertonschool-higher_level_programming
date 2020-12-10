ec878eef41d1d82ed6da06bcc403ee0ab0839481
Natalia Ponsard <2503@holbertonschool.com>
2020-12-10 11:26:32 +0000

    fixed task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
         if argc is 2:
             print("{} argument:".format(argc - 1))
         else:
-            print("{} arguments".format(argc - 1))
+            print("{} arguments:".format(argc - 1))
+
         for i in range(1, argc):
             print("{}: {}".format(i, argv[i]))
