40e5aec2e3653091ad39de68135aec6923bdc591
Natalia Ponsard <2503@holbertonschool.com>
2020-12-15 16:29:30 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print('\n'.join([' '.join(["{:d}".format(item) for item in row])
-                for row in matrix]))
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i is not len(row) - 1:
+                print(" ", end="")
+        print()
