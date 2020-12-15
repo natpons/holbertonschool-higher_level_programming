bc387a6af78cabe89e80d2d5a141eb84feeee9ff
Natalia Ponsard <2503@holbertonschool.com>
2020-12-15 13:22:02 +0000

    fixed task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if (sentence != 0):
         return tuple([len(sentence), sentence[0]])
     else:
-        return tuple([None, 0])
+        return tuple([0, None])
