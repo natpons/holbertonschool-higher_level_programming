60e6f62488057a3f52a4a8632a406a383f52a460
Natalia Ponsard <2503@holbertonschool.com>
2020-12-15 13:01:59 +0000

    fixed task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    first = tuple_a + (0,)
+    first = tuple_a + (0, 0)
     second = tuple_b + (0, 0)
     return(first[0] + second[0], first[1] + second[1])
