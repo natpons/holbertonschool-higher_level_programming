54fe286931d0b53ea27068e5922e4b32522bd884
Natalia Ponsard <2503@holbertonschool.com>
2020-12-17 17:33:55 +0000

    fixed task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key, value_current in a_dictionary.items():
+    new = a_dictionary.copy()
+    for key, value_current in new.items():
         if value_current == value:
             del a_dictionary[key]
 
