ec425a776318407ed45437a6ec3efd3582f3f12d
Natalia Ponsard <2503@holbertonschool.com>
2021-01-04 12:09:54 +0000

    best decision task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    value = None
     try:
         value = a / b
-        return value
     except ZeroDivisionError:
-        return None
+        value = None
     finally:
         print("Inside result:", "{}".format(value))
+        return value
