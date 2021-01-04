b6228c37fd3a1432edce5181aff6d72b27053963
Natalia Ponsard <2503@holbertonschool.com>
2021-01-04 13:20:49 +0000

    task 4 noir

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        value = 0
+        try:
+            value = my_list_1[i] / my_list_2[i]
+        except ValueError:
+            new_list[i] = 0
+        except TypeError:
+            print('wrong type')
+        except ZeroDivisionError:
+            print('division by 0')
+        except IndexError:
+            print('out of range')
+        finally:
+            new_list = value
+    return new_list
