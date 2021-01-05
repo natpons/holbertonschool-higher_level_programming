b077fec5819e5d92afc37bca61241f2d7bb9f744
Natalia Ponsard <2503@holbertonschool.com>
2021-01-05 15:00:09 +0000

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module creates a class Square"""
+
+
+class Square:
+    """
+    This is a class Square that defines a square
+    by a private instance attribute: size
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes class Square with optional size
+        """
+        self.size = size
+
+    def area(self):
+        """
+        Public instance method that returns the current square area
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        to retrieve size - defines size of class Square, returns value of size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ to set value - defines value of class Square size """
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def my_print(self):
+        """ Prints in stdout the square with the character # """
+        if self.size == 0:
+            print()
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
