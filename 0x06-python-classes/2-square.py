758ae3963d1d94adf0f555efd634c603d7268fac
Natalia Ponsard <2503@holbertonschool.com>
2021-01-05 10:28:21 +0000

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module creates a class Square"""
+
+
+class Square:
+    """
+    This is a class Square that defines a square
+    by a private instance attribute: size
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes class Square with optional size
+        """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
