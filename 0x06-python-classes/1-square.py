145a6a187815243d200501e9a6caa65bfbe65e04
Natalia Ponsard <2503@holbertonschool.com>
2021-01-05 10:02:40 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates a class Square"""
+
+
+class Square:
+    """
+    This is a class Square that defines a square
+    by a private instance attribute: size
+    """
+
+    def __init__(self, size):
+        """
+            Initializes class Square by size
+        """
+        self.__size = size
