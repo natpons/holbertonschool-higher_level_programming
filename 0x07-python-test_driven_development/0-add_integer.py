06bbc0883b3d5f2ce6cb5f87e3f5624abe772c6a
Natalia Ponsard <2503@holbertonschool.com>
2021-01-08 16:46:08 +0000

    fixed function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,13 @@ def add_integer(a, b=98):
     """
     Function that adds 2 integers and return the result
     """
+    inf = float('inf')
     if type(a) != int and type(a) != float:
         raise TypeError('a must be an integer')
+    if a == inf:
+        raise TypeError('a must be an integer')
     if type(b) != int and type(b) != float:
+        raise TypeError('b must be an integer')
+    if b == inf:
         raise TypeError('b must be an integer')
     return int(a) + int(b)
