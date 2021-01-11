740f20f6ff381b271282ed100adec589039b4caa
Natalia Ponsard <2503@holbertonschool.com>
2021-01-11 17:30:19 +0000

    changed if condition

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -104,6 +104,6 @@ class Rectangle:
         if isinstance(rect_2, Rectangle) is False:
             raise TypeError('rect_2 must be an instance of Rectangle')
 
-        if rect_1.area() <= rect_2.area():
-            return rect_2
-        return rect_1
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
