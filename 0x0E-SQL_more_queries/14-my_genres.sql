a72a99e91db0417ce6a63402e3c7f84a5c46945d
Natalia Ponsard <2503@holbertonschool.com>
2021-03-03 13:08:14 +0000

    added ORDER BY

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,5 @@
 
 SELECT a.name from tv_genres a
 JOIN tv_show_genres b ON a.id = b.genre_id
-JOIN tv_shows c ON b.show_id = c.id WHERE c.title = "Dexter";
+JOIN tv_shows c ON b.show_id = c.id WHERE c.title = "Dexter"
+ORDER BY a.name ASC;
