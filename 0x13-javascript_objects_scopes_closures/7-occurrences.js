dfcff60ddb858af34c0e2c75bfa5aa9b50503b29
Natalia Ponsard <2503@holbertonschool.com>
2021-03-23 14:51:19 +0000

    added function's comments

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
+  /* executes a reducer function on each element of the array, resulting in single output value */
   return (list.reduce((a, v) => (v === searchElement ? a + 1 : a), 0));
 };
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
+  /* creates a new array with the results of calling a provided function on every element in the calling array */
   return (list.map((item, index) => list[list.length - 1 - index]));
 };
