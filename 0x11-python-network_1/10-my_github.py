3dc0f7ffc99cc15facdb7d848d3edcd4ff005309
Natalia Ponsard <2503@holbertonschool.com>
2021-04-09 17:29:25 +0000

    pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,5 +5,6 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.get("https://api.github.com/user", auth=(argv[1], argv[2])).json()
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(argv[1], argv[2])).json()
     print("{}".format(r.get("id")))
