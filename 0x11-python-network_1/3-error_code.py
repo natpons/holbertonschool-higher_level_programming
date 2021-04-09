2e368aeeaab53e01459c44cda10747b9028ac75d
Natalia Ponsard <2503@holbertonschool.com>
2021-04-09 12:57:09 +0000

    addded req, format

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Script that takes in a URL :
-   - sends a request to the URL,
-   - and displays the body of the response, decoded in utf-8"""
+    - sends a request to the URL,
+    - and displays the body of the response, decoded in utf-8"""
 
 import urllib.request
 import urllib.parse
@@ -10,8 +10,9 @@ from sys import argv
 
 if __name__ == "__main__":
     # https://docs.python.org/3/howto/urllib2.html#httperror
+    req = urllib.request.Request(argv[1])
     try:
-        with urllib.request.urlopen(argv[1]) as response:
+        with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code: {}", e.code)
+        print("Error code: {}".format(e.code))
