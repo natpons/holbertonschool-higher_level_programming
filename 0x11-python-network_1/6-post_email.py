b147c82a5314111055e73f9fa914f0949e2ba2ef
Natalia Ponsard <2503@holbertonschool.com>
2021-04-09 14:45:14 +0000

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that takes in a URL and an email address,
+   - sends a POST request to the passed URL with the email as a parameter,
+   - and finally displays the body of the response"""
+
+import requests
+
+
+if __name__ == "__main__":
+    # https://docs.python-requests.org/en/master/user/quickstart/#more-complicated-post-requests
+    r = requests.post(argv[1], {'email': argv[2]})
+    print(r.text)
