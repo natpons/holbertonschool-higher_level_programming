266c89c174e0207d5299d91fd02fec85d1127f11
Natalia Ponsard <2503@holbertonschool.com>
2021-04-09 15:28:02 +0000

    not be executed when imported

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,10 +8,11 @@ import requests
 from sys import argv
 from requests.exceptions import HTTPError
 
-try:
-    r = requests.get(argv[1])
-    # https://docs.python-requests.org/en/master/user/quickstart/#response-status-codes
-    r.raise_for_status()
-    print(r.text)
-except HTTPError as http_error:
-    print('Error code: {}'.format(r.status_code))
+if __name__ == "__main__":
+    try:
+        r = requests.get(argv[1])
+        # https://docs.python-requests.org/en/master/user/quickstart/#response-status-codes
+        r.raise_for_status()
+        print(r.text)
+    except HTTPError as http_error:
+        print('Error code: {}'.format(r.status_code))
