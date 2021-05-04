9a263c73070a5a1be031e9a48f27ad292c0c3a79
Natalia Ponsard <2503@holbertonschool.com>
2021-05-04 11:32:39 +0000

    error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, function (response, body) {
+request.get(url, function (error, response, body) {
   // Print the response status code if a response was received
   console.log('code:', response && response.statusCode);
 });
