827524d806000c533728fcaa60dac04dc3a8de46
Natalia Ponsard <2503@holbertonschool.com>
2021-05-04 11:24:09 +0000

    del error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-/* Script that display the status code of a GET request */
+// Script that display the status code of a GET request
 
 /* must use the module request */
 const request = require('request');
 const url = process.argv[2];
 
-/**/
-request.get(url, function (error, response, body) {
-  console.log('code:', response && response.statusCode); // Print the response status code if a response was received
+request.get(url, function (response, body) {
+  // Print the response status code if a response was received
+  console.log('code:', response && response.statusCode);
 });
